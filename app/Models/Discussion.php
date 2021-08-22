c2a7de4958d6f44dad3cf929f0b3fc049544c65a
Rajeev12345678 <53248013+Rajeev12345678@users.noreply.github.com>
2021-08-22 23:26:22 +0800

    Initial commit

---
+++ app/Models/Discussion.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Discussion extends Model
+{
+    use HasFactory;
+
+    public function author()
+    {
+      return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function replies()
+    {
+      return $this->hasMany(Reply::class);
+    }
+
+    public function getRouteKeyName()
+    {
+      return 'slug';
+    }
+}
---
+++ resources/views/discussions/show.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="card">
+  @include('partials.discussion-header')
+<div class="card-body">
+  <div class="text-center">
+    <strong>
+      {{$discussion->title}}
+    </strong>
+  </div>
+  <hr>
+  {!!$discussion->content!!}
+</div>
+</div>
+  @foreach($discussion->replies()->paginate(2) as $reply)
+  <div class="card my-5">
+    <div class="card-header">
+      <div class="d-flex justify-content-between">
+        <div>
+          <img width="40px" height="40px" style="border-radius: 50%" src="{{Gravatar::src($reply->owner->email)}}" alt="">
+          <span>{{$reply->owner->name}}</span>
+        </div>
+      </div>
+    </div>
+      <div class="card-body">
+        {!! $reply->content !!}
+    </div>
+  </div>
+
+  @endforeach
+  {{$discussion->replies()->paginate(2)->links('pagination::bootstrap-4')}}
+
+<div class="card my-5">
+  <div class="card-header">
+    Add a reply
+  </div>
+  <div class="card-body">
+    @auth
+    <form action="{{ route('replies.store', $discussion->slug)}}" method="POST">
+      @csrf
+      <input type="hidden" name="content" id="content">
+      <trix-editor input="content"></trix-editor>
+      <button type="submit" class="btn btn-sm my-2 btn-success">
+        Add Reply
+      </button>
+    </form>
+    @else
+    <a href="{{route('login')}}" class="btn btn-info">Sign in to add a reply</a>
+    @endauth
+  </div>
+</div>
+
+@endsection
+@section('css')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.css">
+@endsection
+
+@section('js')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.js"></script>
+@endsection
